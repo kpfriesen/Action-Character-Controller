64530f71def8fb06e7bd0d3ae2fa08b91d132d0a
Kenny Friesen <kpfries@gmail.com>
2021-01-01 19:58:11 -0600

    Wall Running 0.0.2
    wall runs are now controlled by a timer and the character will arc along a curve

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,11 +6,15 @@ export var speed_constant: float = 10
 export var acceleration_constant: float = 5
 export var deceleration_constant: float = 10
 
-export var jump_height: float = 6
+export var jump_height: float = 5
 export var jump_lateral_speed: float = 5
 export var gravity: float = 9.8
+
 export(Curve) var acceleration_curve
 export(Curve) var slide_curve
+export(Curve) var wallrun_profile
+export(Curve) var wallclimb_profile
+
 export var mouse_sensitivity: float = .1
 
 onready var speed: float = speed_constant
@@ -23,10 +27,21 @@ var snap: Vector3 = Vector3(0,0,0)
 var slope_stop: bool = true
 onready var phys_location = global_transform.origin
 
+var wallrun: bool = false
+var sprint: bool = false
+var climbing: bool = false
+var sliding: bool = false
+var can_wall_move: bool = true
+
+
 onready var body = $display
 onready var animation_player = $display/AnimationPlayer
 onready var head = $display/Head
 onready var camera = $display/Head/Camera
+
+#timer references
+onready var wallrun_timer = $Timers/WallRunTimer
+onready var climber_timer = $Timers/WallClimbTimer
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -67,8 +82,9 @@ func crouch():
 
 func wall_angle():
 	if not is_on_floor():
-		var wall_collision = get_slide_collision(0)
-		return wall_collision.normal
+		if is_on_wall():
+			var wall_collision = get_slide_collision(0)
+			return wall_collision.normal
 
 #do this if player inputs crouch at > x% speed
 func slide():
@@ -78,27 +94,53 @@ func slide():
 func vault():
 	pass
 
-#if collider_normal dot movement vector closer to 0
+func wall_listener():
+	if is_on_floor():
+		can_wall_move = true
+	if can_wall_move:
+		if not is_on_floor():
+			if direction.length() > 0:
+				if is_on_wall():
+					var wall_angle = get_slide_collision(0).normal
+					if not wallrun and not climbing:
+						if wall_angle.dot(velocity) > -0.5 and velocity.length() > speed / 2 and climbing == false:
+							wallrun = true
+							wallrun_timer.start()
+						if wall_angle.dot(direction) < -0.7 and wallrun == false:
+							climbing = true
+					wall_reverse()
+					wall_jump()
+			
+
 func wall_run():
-	gravity = 9.8
-	if not is_on_floor():
-		if direction.length() > 0:
-			if is_on_wall():
-				print("is on wall")
-				var wall_angle = get_slide_collision(0).normal
-				print(wall_angle.dot(direction))
-				if wall_angle.dot(direction) > -0.5:
-					velocity += -wall_angle() * 0.1
-					fall_speed = 0
-					return true
+	if wallrun == true:
+		if direction != Vector3(0,0,0) and is_on_wall() and pow(velocity.x, 2) + pow(velocity.z, 2) > .3:
+			var timer_complete = (wallrun_timer.wait_time - wallrun_timer.time_left) / wallrun_timer.wait_time
+			velocity += -wall_angle() * 0.1
+			fall_speed = (wallrun_profile.interpolate(timer_complete) -0.5) * 2
+			return true
+		else:
+			wallrun = false
 
 #do this if wall collider normal dot movement vector is closer to -1
 func wall_climb():
-	pass
+	if climbing == true:
+		if direction.length() > 0.5 and is_on_wall():
+			fall_speed = 5
+			velocity += -wall_angle() * 0.4
+		else:
+			climbing = false
 
 #if wall collider normal dot movement vector is closer to 1
 func wall_jump():
-	pass
+	if Input.is_action_just_pressed("jump"):
+		velocity = (direction.rotated(Vector3(1,0,0), -1 * head.rotation.x))
+		velocity = Vector3(velocity.x * speed, (head.rotation.x/(PI/2) + 0.5) * jump_height, velocity.z * speed)
+		fall_speed = velocity.y
+		print("jump")
+		wallrun = false
+		can_wall_move = true
+
 
 func wall_reverse():
 	if Input.is_action_just_pressed("player_ability_2"):
@@ -160,11 +202,13 @@ func _input(event):
 		head.rotation.x = clamp(head.rotation.x, deg2rad(-89), deg2rad(89))
 		
 func _physics_process(delta):
+	wall_listener()
 	movement_input()
 	crouch()
 	fall(delta)
 	jump()
 	wall_run()
+	wall_climb()
 	movement(delta)
 
 	phys_location = translation
@@ -175,3 +219,13 @@ func _process(delta):
 
 
 
+
+#Timer Signals Here
+func _on_WallRunTimer_timeout():
+	wallrun = false
+	can_wall_move = false
+	print("fall")
+
+
+func _on_WallClimbTimer_timeout():
+	pass # Replace with function body.
